b9f036b11dc0f4b9ac15b32114b60e927215a215
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-02-26 17:58:34 +0300

    separated storage module and improved task logic

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("study tracker project started!!!")
+tasks_list = []
+
+def addTask():
+    title = input("Task title: ")
+    tasks_list.append(title)
+    print("Task added.")
+
+def getTasks():
+    return tasks_list
+
+def showTasks():
+    currentTasks = getTasks()
+
+    if not currentTasks:
+        print("You don't have any tasks.")
+    else:
+        print("\nTasks:")
+        for i, task in enumerate(currentTasks, start=1):
+            print(f"{i}, {task}")
+
+def menu():
+    while True:
+        print("\n---STUDY TRACKER---")
+        print("1. Add Task")
+        print("2. List Tasks")
+        print("3.Exit")
+
+        choice = input("Please select an option: ")
+
+        if choice == "1":
+            addTask()
+        elif choice == "2":
+            showTasks()
+        elif choice == "3":
+            print("Exiting from Study Tracker...")
+            break
+        else:
+            print("Invalid choice!\nYour choice shold be a number!")
+
+
+if __name__ == "__main__":
+    menu()

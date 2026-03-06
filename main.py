d61510a60cfa5afe044dc4d1d24daf694fba98c5
opheliadelrey13 <opheliadelrey13@gmail.com>
2026-03-06 06:25:51 +0300

    Added delete function

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ task_list = loadTasks()
 def addTask(title):
     title = title.strip()
     if not title:
-        return False, "Task title is required"
+        return False, "Task title is required."
     task_list.append(title)
     saveTasks(task_list)
-    return True, "Task added successfully"
+    return True, "Task added successfully."
 
 def showTasks():
     if not task_list:
@@ -19,17 +19,36 @@ def showTasks():
         output += f"{i}. {task}\n"
     return output
 
+
+def deleteTask(task_number):
+    if not task_list:
+        return False, "You don't have any tasks."
+    if not str(task_number).isdigit():
+        return False, "Please enter a valid task number."
+
+    task_number = int(task_number)
+
+    if task_number < 1 or task_number > len(task_list):
+        return False, "Task number doesn't exist."
+
+    removedTask = task_list.pop(task_number - 1)
+    saveTasks(task_list)
+    return True, f"Task {removedTask} deleted successfully."
+
+
+
 def menu():
     while True:
         print("\n--- STUDY TRACKER ---")
         print("1. Add Task")
         print("2. List Tasks")
-        print("3. Exit")
+        print("3. Delete Task")
+        print("4. Exit")
 
         choice = input("Please select an option: ").strip()
 
         if choice == "1":
-            title = input("Task title: ").strip().upper()
+            title = input("Task title: ").strip()
             success, message = addTask(title)
             print(message)
 
@@ -37,8 +56,14 @@ def menu():
             print(showTasks())
 
         elif choice == "3":
-            print("Exiting from Study Tracker...")
+            print(showTasks())
+            task_number = input("Enter the task number you want to delete: ").strip()
+            success, message = deleteTask(task_number)
+            print(message)
+        elif choice == "4":
+            print("Exiting from the Study Tracker...")
             break
+
 
         else:
             print("Invalid choice! Your choice should be 1, 2, or 3.")
